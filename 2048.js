7fa21bdf40b7ab81f2f114ce89de76c7abc6fde5
YasseRRRRRRRR <yassermoussaddak3@gmail.com>
2023-11-18 23:44:34 +0200

    dont with left noe do the same for the othe keys

--- 2048.js
+++ 2048.js
@@ -8,12 +8,19 @@ window.onload = function () {
 }
 
 function setGame() {
+    // test
     board = [
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
-    ]
+        [2, 2, 2, 2],
+        [2, 2, 2, 2],
+        [4, 4, 8, 8],
+        [4, 4, 8, 8]
+    ];
+    // board = [
+    //     [0, 0, 0, 0],
+    //     [0, 0, 0, 0],
+    //     [0, 0, 0, 0],
+    //     [0, 0, 0, 0]
+    // ]
 
     for (let r = 0; r < row; r++) {
         for (let c = 0; c < columns; c++) {
@@ -30,18 +37,69 @@ function updateTile(tile, num) {
     tile.innerText = "";
     tile.classList.value = "";
     tile.classList.add("tile");
-    if(num>0) {
+    if (num > 0) {
         tile.innerText = num;
         if (num <= 1024) {
-            tile.classList.add("x"+num.toString());
+            tile.classList.add("x" + num.toString());
         }
-        else{
+        else {
             tile.classList.add("x2048");
 
         }
     }
 }
 
+document.addEventListener("keyup", (e) => {
+    if (e.code == "ArrowLeft") {
+        slideLeft();
+    }
+    else if (e.code == "ArrowRight") {
+        slideRight();
+    }
+    else if (e.code == "ArrowUp") {
+        slideUp();
+    }
+    else if (e.code == "ArrowDown") {
+        slideDown();
+    }
+    document.getElementById("score").innerText = score;
+})
 
+function filterZero(row) {
+    return row.filter(num => num != 0);
+}
+
+function slide(row) {
+    row = filterZero(row);
+
+    for (let i = 0; i < row.length - 1; i++) {
+        if (row[i] == row[i + 1]) {
+            row[i] *= 2;
+            row[i + 1] = 0;
+            score += row[i];
+        }
+    }
+
+    row = filterZero(row);
+
+    while (row.length < columns) {
+        row.push(0);
+    }
+
+    return row;
+}
+
+function slideLeft() {
+    for (let r = 0; r < row; r++) {
+        let row = board[r];
+        row = slide(row);
+        board[r] = row;
+        for (let c = 0; c < columns; c++) {
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let num = board[r][c];
+            updateTile(tile, num);
+        }
+    }
+}
 
 
